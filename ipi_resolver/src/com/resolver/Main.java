bcde98d205bc68b0e987f983e63f650f740904a7
Breno Van Dall <vbreno335@gmail.com>
2025-09-06 18:25:04 -0300

    feats

--- ipi_resolver/src/com/resolver/Main.java
+++ ipi_resolver/src/com/resolver/Main.java
@@ -6,15 +6,18 @@ public class Main {
     private static final XmlServiceImpl xmlService = new XmlServiceImpl();
 
     public static void main(String[] args) throws Exception {
-        checkArgs(args);
+        if (!checkArgs(args)) return;
         xmlService.doOperation(args[0], args[1]);
     }
 
-    private static void checkArgs(String[] args) {
+    private static boolean checkArgs(String[] args) {
         if (args.length < 2) {
             System.out.println("Usabilidade: ipi_resolver [0] [1]");
             System.out.println("   [0]  caminho físico do diretório que contém os arquivos XML");
             System.out.println("   [1]  caminho físico do arquivo .csv da tabela TIPI");
+
+            return false;
         }
+        return true;
     }
 }
